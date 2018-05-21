3ccb71a910782bd7bf18dbf3cd01c85cd711c35c
DESKTOP-UJNPOJ2\sicko <sickogagi>
2018-05-21 12:55:39 +0200

    no message

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float cameraHeight = 20.0f;
+    public float cameraBehind = 10f;
+    void Update()
+    {
+
+        Vector3 pos = player.transform.position;
+        pos.z += cameraBehind;
+        pos.y += cameraHeight;
+
+        transform.position = pos;
+               
+    }
+}
+  
+
---
+++ Assets/CatapultController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CatapultController : MonoBehaviour {
+
+    public Camera ControlCamera;
+
+    public Camera AmmoCamera;
+
+    public Slider TensionSlider;
+
+    public GameObject Arm;
+
+    public SpringJoint FiringSpring;
+
+    public SpringJoint RestraintSpring;
+
+    public List<SpringJoint> SpringJoints;
+    public bool camSwitch = true;
+
+    private void Start()
+    {
+        SpringJoints.AddRange(Arm.GetComponents<SpringJoint>());
+
+        for (int i = 0; i < SpringJoints.Capacity; i++)
+        {
+            if(SpringJoints[i].connectedBody.gameObject.name == "Anchor2")
+            {
+                FiringSpring = SpringJoints[i];
+            }
+            else
+            {
+                RestraintSpring = SpringJoints[i];
+            }
+        }
+    }
+
+    void ResetCatapult()
+    {
+
+    }
+
+    public void SwitchCamera()
+    {
+        camSwitch = !camSwitch;
+        ControlCamera.gameObject.SetActive(camSwitch);
+        AmmoCamera.gameObject.SetActive(!camSwitch);
+    }
+
+    public  void fireCatapult()
+    {
+        Destroy(RestraintSpring);
+    }
+    
+    public void GetTension(float Tension)
+    {
+        FiringSpring.spring = TensionSlider.value;
+    }
+}
